23a55d2e050902a7eaf69a56af7483248ad223dd
DivyaKumarBaid <divyakumarbaid.dkb.2001@gmail.com>
2022-06-30 20:27:27 +0530

    home

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import NavBar from './components/NavBar';
 import Overlay from './components/Overlay';
 import ScrollOut from "scroll-out";
-import {useEffect} from "react"
+import { useEffect } from "react"
+import Home from './components/Home'
 
 
 
@@ -10,14 +11,15 @@ function App() {
 
   useEffect(() => {
     ScrollOut({
-      targets: '.navLinks,.navLogo'
+      targets: '.navLinks,.navLogo,.home'
     });
   },[]);
 
   return (
     <div className="App">
       <Overlay />
-      <NavBar/>
+      <NavBar />
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='home'>
+            <div className="details">
+                <div className="greet">
+                    Namaste,
+                </div>
+                <div className="name">
+                    Divya Kumar Baid 
+                <span className='here'>Here</span>
+                </div>
+                <div className="occu">
+                    I am a WEB Explorer.
+                </div>
+                <div className="desc">
+                    I'm a Full-Stack Developer and a maniac when it comes to Tech. I build things for WEB and design occasionally.
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
