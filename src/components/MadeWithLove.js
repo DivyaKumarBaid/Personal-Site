7ac31eb537401f19c02a6d3f81b14127e14e2e1f
DivyaKumarBaid <divyakumarbaid.dkb.2001@gmail.com>
2022-07-05 22:20:03 +0530

    added github link of website

--- src/components/MadeWithLove.js
+++ src/components/MadeWithLove.js
@@ -3,7 +3,8 @@ import React from 'react';
 const MadeWithLove = () => {
     return (
         <div className='made'>
-            Made with ❤️ by Divya Kumar Baid
+            <div className='tag'>Made with ❤️ by Divya Kumar Baid</div>
+            <div className='repoLink'><a href=""><span>Github Link</span></a></div>
         </div>
     );
 }
