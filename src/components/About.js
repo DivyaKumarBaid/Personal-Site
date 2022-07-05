86706798d7001e464c352f15ecc35dab34096768
DivyaKumarBaid <divyakumarbaid.dkb.2001@gmail.com>
2022-07-05 22:43:19 +0530

    Highlighted

--- src/components/About.js
+++ src/components/About.js
@@ -13,14 +13,14 @@ export default function About(props) {
                 <div className="hrLine"></div>
             </div>
               <div className="aboutPara">
-                Hi, my name is <span className="highlight"> Divya Kumar Baid</span> and I find WEB a cause for living as it is live. I am currently pursuing B.Tech in Computer Science and Engineering from Techno Main Salt Lake, India. I crave for problems related to Tech and enjoy solving them. I found my passion right when we were being taught HTML & CSS in Middle School. Back then I really did wonder how google works.
+                Hi, my name is <span className="highlight"> Divya Kumar Baid</span> and I find WEB a cause for living as it is live. I am currently pursuing <span className="highlight">B.Tech in Computer Science and Engineering</span> from Techno Main Salt Lake, India. I crave for problems related to Tech and enjoy solving them.
                 <br/>
                 <br/>
-          Fast Forward to today I am a working member of a student led Technical Club CUrBrain of Techno Main Salt Lake. I have participated in several Hackathons and have experience in Full Stack Development using multiple tools and frameworks.
-          <br/>
-          <br/>
-          Some of the Tech Stack and tools that I have worked with are :
-          <br />
+                Fast Forward to today I am a working member of a student led <span className="highlight">Technical Club CUrBrain</span> of Techno Main Salt Lake. I have participated in several Hackathons and have experience in Full Stack Development using multiple tools and frameworks.
+                <br/>
+                <br/>
+                Some of the Tech Stack and tools that I have worked with are :
+                <br />
           <div className="toolsList">
             <ol>
               <li>C++</li>
