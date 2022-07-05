2d8183e355360c97e2bfbc62508765587d503250
DivyaKumarBaid <divyakumarbaid.dkb.2001@gmail.com>
2022-07-05 22:05:11 +0530

    added project info

--- src/components/project.js
+++ src/components/project.js
@@ -1,38 +1,38 @@
 const project = [
     {
         name: "Discord-Bot",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio consectetur labore architecto quidem aspernatur expedita ipsa?",
-        link: "www.google.com",
-        tech: ["VS-CODE","PYTHON","DISCORD-PY"],
-        image:"/assets/page.png"
+        description: "A bot that plays your created playlist in loop. Just type the name of the song with the command prefix to add it and play it out loud!",
+        link: "https://github.com/DivyaKumarBaid/Muso",
+        tech: ["PYTHON","DISCORD-PY"],
+        image:"/assets/discord.gif"
     },
     {
-        name: "Discord-Bot",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio consectetur labore architecto quidem aspernatur expedita nesciunt nostrum ea accusantium nam iste ut eos laudantium, exercitationem enim obcaecati impedit necessitatibus ipsa?",
-        link: "www.google.com",
-        tech: ["VS-CODE","PYTHON","DISCORD-PY"],
-        image:"/assets/page.png"
+        name: "EDG.CO.IN",
+        description: "A Website made using MERN STACK that was used by 2000+ people. It has a 3D model made using spline.design a framework based on threeJS. The site was used for event registration of EDGE 2022 Fest of Geekonix TMSL.",
+        link: "edg.co.in",
+        tech: ["REACT","THREEJS","SPLINE.DESIGN","NODE-JS","MONGO-DB","EXPRESS"],
+        image:"/assets/edge.gif"
     },
     {
-        name: "Discord-Bot",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio consectetur labore architecto quidem aspernatur expedita nesciunt nostrum ea accusantium nam iste ut eos laudantium, exercitationem enim obcaecati impedit necessitatibus ipsa?",
-        link: "www.google.com",
-        tech: ["VS-CODE","PYTHON","DISCORD-PY"],
-        image:"/assets/page.png"
+        name: "SLOTH",
+        description: "A social media web-application built specially for aspiring programmers. This is an online platform to post your own coding experience and help others",
+        link: "https://github.com/DivyaKumarBaid/Sloth_backend",
+        tech: ["REACT", "PYTHON", "FASTAPI", "MONGO-DB"],
+        image:"/assets/sloth.gif"
     },
     {
-        name: "Discord-Bot",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio consectetur labore architecto quidem aspernatur expedita nesciunt nostrum ea accusantium nam iste ut eos laudantium, exercitationem enim obcaecati impedit necessitatibus ipsa?",
-        link: "www.google.com",
-        tech: ["VS-CODE","PYTHON","DISCORD-PY"],
-        image:"/assets/page.png"
+        name: "WELL-BEING",
+        description: "A Web App that allows user to have appointments with the nearest doctor. This app builds a community of medical service providers and acts as a point of contact between patients and the doctors/hospitals .",
+        tech: ["REACT", "PYTHON", "FASTAPI", "MONGO-DB"],
+        link:"https://github.com/DivyaKumarBaid/Well-Being",
+        image:"/assets/wellbeing.gif"
     },
     {
-        name: "Discord-Bot",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio consectetur labore architecto quidem aspernatur expedita nesciunt nostrum ea accusantium nam iste ut eos laudantium, exercitationem enim obcaecati impedit necessitatibus ipsa?",
-        link: "www.google.com",
-        tech: ["VS-CODE","PYTHON","DISCORD-PY"],
-        image:"/assets/page.png"
+        name: "DEV-BLOGS",
+        description: "My First FullStack application right after when I have started using React and FastAPI. This was just for practice but this had my own share of learning experience and thus it is here",
+        tech: ["REACT", "PYTHON", "FASTAPI", "MONGO-DB"],
+        link:"https://github.com/DivyaKumarBaid/blog_react",
+        image:"/assets/blog.gif"
     },
 
 ];
